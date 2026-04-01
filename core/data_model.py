0bb3867c971d32a831f320b9c64d0c6a90cbcca4
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-04-02 00:06:38 +0200

    created data model for storing resume,education, profession exp and job details

--- core/data_model.py
+++ core/data_model.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+
+from pydantic import BaseModel,Field,HttpUrl
 
 
 #class to store education details
@@ -8,19 +9,29 @@ class EducationDataModel(BaseModel):
     
 # class to store professional experience
 class ProfExpDataModel(BaseModel):
+
     post: str
-    years: int
+    years: str
     summary_of_job: str
     
     
 # class to store resume details
 class ResumeDataModel(BaseModel):
+    is_fresher:bool=Field(default=False)
     education: list[EducationDataModel]
-    professional_exp: list[ProfExpDataModel] | None=None
+    professional_exp: list[ProfExpDataModel] = Field(default=[])
     skills: list[str]
     summary: str | None=None
     
-    
-    
-    
 
+
+class JobDetailsDataModel(BaseModel):
+    company_name: str
+    title : str
+    location : str
+    job_type : str
+    description : str
+    requirements : list[str]
+    salary :str = Field(default="Not available")
+    url : HttpUrl
+    
