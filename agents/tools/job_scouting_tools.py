765f08678b2b0f36a489e301b10f10371295458d
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-04-03 21:15:45 +0200

    Created tools for job scouting agents

---
+++ agents/tools/job_scouting_tools.py
@@ -0,0 +1,52 @@
+from langchain.tools import tool
+from jobspy import scrape_jobs
+from core.data_model import JobDetailsDataModel
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+GROQ_API_KEY=os.getenv('GROQ_API_KEY')
+
+
+SITE_NAMES=["indeed", "linkedin", "zip_recruiter", "google" , "glassdoor", "bayt", "naukri", "bdjobs"]
+
+@tool
+def find_jobs(keywords: list,locations:list):
+    """
+    Based on the keywords and location
+    Find jobs at these location in the fields specified in keywords
+    :param keywords:
+    :param location:
+    :return: json
+    """
+    location=",".join(locations)
+    search_term = ",".join(keywords)
+    
+    
+    jobs=scrape_jobs(site_name=SITE_NAMES,
+                           search_term=search_term,
+                           location=location,
+                           results_wanted=25)
+    
+    
+    #manually converting each row of data frame to pydantic object
+    found_jobs=[]
+    for _,row in jobs.iterrows():
+        job = JobDetailsDataModel(
+            company_name=row['COMPANY'],
+            title=row['TITLE'],
+            location=row['CITY'],
+            job_type=row['JOB_TYPE'],
+            url=row['JOB_URL'],
+            description=row['DESCRIPTION'],
+            salary=str(row['MIN_AMOUNT']) if row['MIN_AMOUNT'] else "Not available"
+        )
+        
+        found_jobs.append(job)
+    
+    
+    
+    return found_jobs
+
+
